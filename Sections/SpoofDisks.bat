7bed152cf9d423b29758eb1fed40546ba4229b33
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2023-07-04 18:48:09 +0000

    Add files via upload

--- Sections/SpoofDisks.bat
+++ Sections/SpoofDisks.bat
@@ -19,22 +19,6 @@ fltmc >nul 2>&1 || (
 
 
 :: ====================================================================================================
-:: 20 Digit long "a-z, A-Z, 0-9" random string generator
-:: ====================================================================================================
-
-set "characters=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-set "length=20"
-set "randomString="
-
-for /L %%i in (1,1,%length%) do (
-    set /A "index=!random! %% 62"
-    for %%j in (!index!) do set "randomString=!randomString!!characters:~%%j,1!"
-)
-
-:: ====================================================================================================
-
-
-:: ====================================================================================================
 :: Physical Drives (SSD/HDD) - Serial Number(s) | Reset Physical Disk Status(es)
 :: ====================================================================================================
 
@@ -45,6 +29,7 @@ for /L %%i in (1,1,%length%) do (
 		for /f "delims=:" %%B in ("%%A") do (
 			for /l %%C in (0,1,%%B) do (
 				if "%%A"=="%%B" (
+					call :20HEX
 					reg add "HKLM\HARDWARE\DEVICEMAP\Scsi\Scsi Port %%A\Scsi Bus 0\Target Id 0\Logical Unit Id 0" /v "SerialNumber" /t REG_SZ /d "!randomString!" /f
 				)
 			)
@@ -52,5 +37,23 @@ for /L %%i in (1,1,%length%) do (
 	)
 	powershell Reset-PhysicalDisk *
 )
+
+:: ====================================================================================================
+
+
+:: ====================================================================================================
+:: 20 Digit long "a-z, A-Z, 0-9" random string generator
+:: ====================================================================================================
+
+:20HEX
+set "characters=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+set "length=20"
+set "randomString="
+
+for /L %%i in (1,1,%length%) do (
+    set /A "index=!random! %% 62"
+    for %%j in (!index!) do set "randomString=!randomString!!characters:~%%j,1!"
+)
+exit /b 0
 
 :: ====================================================================================================
