48c8b591835a3d9faca3512de4b32b38d9bd34b2
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2024-03-19 04:59:48 +0000

    Add files via upload

---
+++ PS_Modules/Name/name.ps1
@@ -0,0 +1,14 @@
+$RandomString = -join ((48..57) + (65..90) | Get-Random -Count '7' | % {[char]$_})
+
+# Local Windows User Account
+# $env:USERNAME
+# Rename-LocalUser -Name "$env:USERNAME" -NewName "$RandomString$RandomString"
+
+# Local Computer Name (Device Name)
+# [System.Environment]::MachineName
+# Rename-Computer -NewName "DESKTOP-$RandomString" -Force *>$null
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName' -Name 'ComputerName' -Type String -Value "DESKTOP-$RandomString" -Force
+
+# Network Computer Name (NetBIOS Name)
+# [System.Net.Dns]::GetHostName()
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' -Name 'ComputerName' -Type String -Value "DESKTOP-$RandomString" -Force
