09e3675da42c95b0f0aa32677a0dd783df20e609
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2024-04-11 06:31:21 +0000

    Update name.ps1

--- PS_Modules/Name/name.ps1
+++ PS_Modules/Name/name.ps1
@@ -6,9 +6,9 @@ $RandomString = -join ((48..57) + (65..90) | Get-Random -Count '7' | % {[char]$_
 
 # Local Computer Name (Device Name)
 # [System.Environment]::MachineName
-# Rename-Computer -NewName "DESKTOP-$RandomString" -Force *>$null
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName' -Name 'ComputerName' -Type String -Value "DESKTOP-$RandomString" -Force
+Rename-Computer -NewName "DESKTOP-$RandomString" -Force *>$null
+# Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName' -Name 'ComputerName' -Type String -Value "DESKTOP-$RandomString" -Force
 
 # Network Computer Name (NetBIOS Name)
 # [System.Net.Dns]::GetHostName()
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' -Name 'ComputerName' -Type String -Value "DESKTOP-$RandomString" -Force
+# Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' -Name 'ComputerName' -Type String -Value "DESKTOP-$RandomString" -Force
