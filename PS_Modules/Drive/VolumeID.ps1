d98935b2c064bb2876df6b18384ae9d2a5733bab
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2024-10-03 17:12:35 -0500

    Update VolumeID.ps1

--- PS_Modules/Drive/VolumeID.ps1
+++ PS_Modules/Drive/VolumeID.ps1
@@ -1,27 +1,29 @@
 function Get-UpperRandomString {
-    $RandomString = -join (1..8 | ForEach {[char]((65..71) + (48..57) | Get-Random)})
-	$FormattedRandomString = ($RandomString -replace '(.{4})(.{4})', '$1-$2')
-    return $FormattedRandomString
+    $randomChars = 65..71 + 48..57
+    $randomString = -join (1..8 | ForEach-Object { [char](Get-Random -InputObject $randomChars) })
+    return ($randomString -replace '(.{4})(.{4})', '$1-$2')
 }
 
+$tempPath = $env:temp
+$volumeIdPath = "$tempPath\Volumeid64.exe"
+$zipPath = "$tempPath\VolumeId.zip"
+$testIP = "9.9.9.9"
 
-if (-not (Test-Path "$env:temp\Volumeid64.exe")) {
-    $pingResult = Test-Connection -ComputerName 9.9.9.9 -Count 1 -Quiet
-
-    if (-not $pingResult) {
+if (-not (Test-Path $volumeIdPath)) {
+    if (-not (Test-Connection -ComputerName $testIP -Count 1 -Quiet)) {
         Write-Host "No internet connection"
         exit 1
     }
 
-    Set-Location -Path $env:temp
+    Set-Location -Path $tempPath
     Invoke-WebRequest -Uri "https://download.sysinternals.com/files/VolumeId.zip" -OutFile "VolumeId.zip" -UserAgent "Mozilla/5.0" -UseBasicParsing
-    Expand-Archive -Path "$env:temp\VolumeId.zip" -DestinationPath "$env:temp"
+    Expand-Archive -Path $zipPath -DestinationPath $tempPath -Force
 
-    .\Volumeid64.exe | ForEach-Object {
-        $driveLetter = $_.Substring(0, 1)
-        $volumeId = "$($_.Substring(1, 4))-$($_.Substring(5, 4))"
+    Get-ChildItem -Path "$tempPath\Volumeid64.exe" | ForEach-Object {
+        $driveLetter = $_.Name.Substring(0, 1)
+        $volumeId = "$($_.Name.Substring(1, 4))-$($_.Name.Substring(5, 4))"
         if (Test-Path "$driveLetter:\") {
-            Start-Process -FilePath "$env:temp\Volumeid64.exe" -ArgumentList "$driveLetter:", $volumeId, "-nobanner" -NoNewWindow -Wait
+            Start-Process -FilePath $volumeIdPath -ArgumentList "$driveLetter:", $volumeId, "-nobanner" -NoNewWindow -Wait
         }
     }
 }
