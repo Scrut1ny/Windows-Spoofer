3b9a676f57c74be7199d9c33415eab056170b837
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-02-16 13:09:00 +0300

    Update guids.ps1

--- PS_Modules/GUID/guids.ps1
+++ PS_Modules/GUID/guids.ps1
@@ -1,19 +1,15 @@
-function Get-RandomGuid {
-    return [guid]::NewGuid().ToString()
-}
-
 function Get-RandomProductId {
     return (-join (1..20 | ForEach {[char]((65..90) + (48..57) | Get-Random)}) -replace '(.{5})', '$1-').TrimEnd('-')
 }
 
 # HwProfileGuid
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001' -Name 'HwProfileGuid' -Type String -Value "{$(Get-RandomGuid)}" -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001' -Name 'HwProfileGuid' -Type String -Value "{$([guid]::NewGuid().ToString())}" -Force
 
 # Cryptography
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Cryptography' -Name 'MachineGuid' -Type String -Value "$(Get-RandomGuid)" -Force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Cryptography' -Name 'MachineGuid' -Type String -Value "$([guid]::NewGuid().ToString())" -Force
 
 # SQMClient (Device ID)
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\SQMClient' -Name 'MachineId' -Type String -Value "{$(Get-RandomGuid).ToUpper()}" -Force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\SQMClient' -Name 'MachineId' -Type String -Value "{$([guid]::NewGuid().ToString().ToUpper())}" -Force
 
 # ProductId (Product ID)
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name 'ProductId' -Type String -Value "$(Get-RandomProductId)" -Force
